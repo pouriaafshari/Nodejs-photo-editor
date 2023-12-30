3967a1efcf5ef849baa3b63284a190d1d7342c24
pouriaafshari <pooriyaafshari81@gmail.com>
2023-12-30 01:17:31 +0100

    login testing

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://karina:hello123456@cluster0.hmwtw7f.mongodb.net/?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -15,80 +12,59 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+// Connect to MongoDB when the server starts
+client.connect(err => {
+  if (err) {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
+  }
+  console.log('Connected to MongoDB');
+});
+
 async function findUser(uname) {
-    try {
-        // Get the database and collection on which to run the operation
-        const database = client.db("react-photo-editor");
-        const users = database.collection("users");
-        // Query for a movie that has the title 'The Room'
-        const query = { username: uname };
-        // Execute query
-        const user = await users.findOne(query)
-        return user;
-    } finally {
-        await client.close();
-    }
+  const database = client.db("react-photo-editor");
+  const users = database.collection("users");
+  const query = { username: uname };
+  const user = await users.findOne(query);
+  return user;
 }
 
-run("poorei").catch(console.dir);
-
-
-
-
-// Define a Mongoose schema for users
-const userSchema = new mongoose.Schema({
-    username: { type: String, unique: true, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true }
-});
-
-
-app.post('/register', async (req, res) => {
-    try {
-        // Check if the username already exists in the database
-        const existingUser = await User.findOne({ username: req.body.username });
-        if (existingUser) {
-            return res.status(400).send('Username already exists');
-        }
-
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
-        // Create a new user instance based on the Mongoose model
-        const newUser = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hashedPassword
-        });
-
-        // Save the new user to the database
-        await newUser.save();
-        res.status(201).send();
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
 app.post('/login', async (req, res) => {
-    try {
-        // Find the user in the database by username
-        const user = await findUser(req.body.username);
-        if (!user) {
-            return res.status(400).send('User not found');
-        }
-
-        // Compare the provided password with the hashed password in the database
-        const passwordMatch = await bcrypt.compare(req.body.password, user.password);
-
-        if (passwordMatch) {
-            res.send('Success');
-        } else {
-            res.send('Wrong Password');
-        }
-    } catch (error) {
-        res.status(500).send(error.message);
+  try {
+    const user = await findUser(req.body.username);
+    if (!user) {
+      return res.status(400).send('User not found');
     }
+    return res.status(200).send('yes')
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
-app.listen(3000, () => {
-    console.log("It's on");
+// Handle MongoDB reconnection events
+client.on('reconnect', () => {
+    console.log('Reconnected to MongoDB');
+  });
+  
+  client.on('close', () => {
+    console.log('Connection to MongoDB closed');
+  });
+  
+  client.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+  });
+
+// Close the MongoDB client when the server stops
+process.on('SIGINT', () => {
+  client.close().then(() => {
+    console.log('MongoDB client disconnected');
+    process.exit(0);
+  });
 });
+
+// Start the server
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+  
